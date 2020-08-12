68da10bae115d47edb65f8ef4c066d061bf3196d
Alex <alexmoller02@hotmail.com>
2020-08-12 10:57:48 +0200

    Updates

---
+++ precache-manifest.755064bc0376495031984627ab3be74b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc3666ddc474f509c0567f98aa90b61",
+    "url": "/snakeproject/index.html"
+  },
+  {
+    "revision": "7eacda091b073c6a85be",
+    "url": "/snakeproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "798619802a655bf8faad",
+    "url": "/snakeproject/static/js/2.48ec697e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snakeproject/static/js/2.48ec697e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eacda091b073c6a85be",
+    "url": "/snakeproject/static/js/main.6480e942.chunk.js"
+  },
+  {
+    "revision": "9046bdc94a3aafa19a5c",
+    "url": "/snakeproject/static/js/runtime-main.2ce3b7af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/snakeproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
